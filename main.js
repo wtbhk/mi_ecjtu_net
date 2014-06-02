dad1c2347fefb28d1a53621395bcfe10f3cb8215
wtbhk <wtbhk@live.cn>
2014-06-02 19:55:35 +0800

    Signed-off-by: wtbhk <wtbhk@live.cn>

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$.get("http://mi.ecjtu.net/list?count=20&start_from=0", function(result){
 		for(var i=0;i<result.length;i++){
 			var tmpl = [
-			'						<a href="javascript:void(0)"><li class="main">',
+			'						<a href="javascript:void(0)"><li class="main" id="'+result['id']+'">',
 			'				<div class="author">',
 								result[i]['author'],
 			'				</div>',
@@ -17,6 +17,7 @@ $(document).ready(function(){
 	},'json');
 });
 
+$
 
 function calcColor(vote){
 	var str = '#';
@@ -27,51 +28,4 @@ function calcColor(vote){
 	str += g < 10 ? "0" + g.toString(16) : g.toString(16);
 	str += b < 10 ? "0" + b.toString(16) : b.toString(16);
 	return str;
-}
-	/**
-	 * [parserColor 颜色转换为十六进制]
-	 * @param  {[String]} value [需要转换的颜色值]
-	 * @return {[String]}       [返回转换后的颜色值,#0000FF形式]
-	 */
-function parserColor (value){
-	var
-	str      = "",
-	arr      = [],
-	arri     = "",
-	i        = 0,
-	vlen     = value.length,
-	colorObj = {
-		"black":"000000",
-		"red":"0000FF",
-		"blue":"FF0000",
-		"white":"FFFFFF",
-		"yellow":"FFFF00",
-		"orange":"FFA500"
-	};
-
-	//rgb(0,0,255)
-	if(/rgb/.test(value)){
-		arr = value.match(/\d+/g);
-		vlen = arr.length;
-		for(; i < vlen ; i++){
-			arri = parseInt(arr[i]);
-			//转换为十六进制
-			str += arri < 10 ? "0" + arri.toString(16) : arri.toString(16);
-		}
-	}else if(/^#/.test(value)){
-		//#00f
-		if(vlen == 4){
-			str = value.replace(/[A-Za-z0-9]/g,"$&$&");
-		}else if(vlen == 7){
-			//#FF0000
-			str = value.replace(/^#([A-Za-z0-9]*)/,"$1");
-		}else{
-			str = "FFFFFF";
-		}
-	}else{
-		//red/orange
-		value = value.toLowerCase();
-		str = colorObj[value] ? colorObj[value] : "FFFFFF";//不匹配默认为白色
-	}
-	return "#" + str.toUpperCase();
 }
