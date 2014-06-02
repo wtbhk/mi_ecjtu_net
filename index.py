687ca8c11699f5d5b44030aa1f9ec09abeac70d6
wtbhk <wtbhk@live.cn>
2014-06-02 18:06:34 +0800

    Signed-off-by: wtbhk <wtbhk@live.cn>

--- index.py
+++ index.py
@@ -34,7 +34,7 @@ def new():
 	if pattern.match(request.form['content']) or pattern.match(request.form['author']):
 		return json.dumps({'result':'error'})
 	id = r.incr('mi_id')
-	result = r.pipline().hset('mi_'+str(id), 'author', request.form['author']).hset('mi_'+str(id), 'content', request.form['content']).hset('mi_'+str(id), 'vote', 0).zadd('mi_list', time.time(), id).zadd('mi_vote', 0, id).execute()
+	result = r.pipeline().hset('mi_'+str(id), 'author', request.form['author']).hset('mi_'+str(id), 'content', request.form['content']).hset('mi_'+str(id), 'vote', 0).zadd('mi_list', time.time(), id).zadd('mi_vote', 0, id).execute()
 	if result:
 		return json.dumps({'result':'success', 'id':id})
 
