36be75200bedb31723d582787cfce9d5792469ee
wtbhk <wtbhk@live.cn>
2014-06-02 16:59:57 +0800

    Signed-off-by: wtbhk <wtbhk@live.cn>

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-	return 'hello';
+@app.route('/list')
+def list():
+	return request.args.get('count')
+
+
+@app.route('/<int:id>/vote')
+def vote(id):
+	return id
+
+
+@app.route('/new', methods=['POST'])
+def new():
+	return request.form
+
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
