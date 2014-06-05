db80162d406579225e70a231034f83e02b3f5184
wtbhk <wtbhk@live.cn>
2014-06-05 22:12:26 +0800

    Signed-off-by: wtbhk <wtbhk@live.cn>

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ $(document).ready(function(){
 			redraw();
 		});
 	});
-	$(".new .form button").on('click', function(){
+	$(".new .form button").click( function(){
 		var author = $("#college option:selected").text() + $("#grade option:selected").text();
 		var content = $("#content").val();
 		$.post("http://mi.ecjtu.net/new", {'author':author, 'content':content}, function(result){
 			redraw();
 		});
-		event.stopPropagation();
 	});
 });
 
